395b21ea88dbe7d787380ce989778663818bd5d6
yporto <yporto@pryntec.com>
2024-07-30 14:13:33 +0200

    few_few_babel

--- renderer/map_to_babel.py
+++ renderer/map_to_babel.py
@@ -17,3 +17,36 @@ few_babel = {
     86: 86, #drink
     113: 113 #open something
 }
+
+few_few_babel = {
+    15: 15, #stretch
+    16: 16, #jump
+    19: 19, #place something
+    21: 21, #run
+    23: 23, #throw
+    26: 26, #stand up
+    30: 30, #action with ball
+    31: 31, #kick
+    37: 37, #lift something
+    52: 52, #squat
+    62: 62, #shake
+    64: 64, #clap
+}
+
+ntu2babel_few_few = {
+    6: 23, #throw
+    # 7: 18, #sitting down
+    8: 26, #standing up (from sitting position)
+    9: 64, #clapping
+    23: 31, #kicking something
+    26: 16, #jump up
+    50: 31, #kicking other person
+    63: 30, #bounce ball
+    79: 52, #squat down
+    91: 37, #lift something
+    92: 62, #shake fist
+    98: 21, #running on the spot
+    99: 31, #butt kicks (kick backward)
+    101: 31, #side kick
+    103: 15 #stretch oneself
+}
